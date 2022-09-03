d13e64241c6454661194053ecf298fbf3a0eedd1
Paul Vaughan <paulieboo@gmail.com>
2022-09-03 23:00:19 +0100

    The absolute most basic clicker: it adds one to a variable and updates the page. Does not survive a page reload.

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 A small project to learn a little JavaScript and make a dumb idle-clicker style game.
 
 
+## How to use
+
+Load the `game.html` file into your browser. Experience the awesomeness. 
+
+
 ## Files
 
 * `game.html`: bare minimum HTML required to use Bootstrap 5.2 and load the `game.js` file.
@@ -12,3 +17,9 @@ A small project to learn a little JavaScript and make a dumb idle-clicker style 
 ## History
 
 * **v0.0.0**: The absolute bare minimum HTML and JavaScript required to 1. [use Bootstrap 5.2](https://getbootstrap.com/docs/5.2/getting-started/introduction/), and 2. ensure console.log() works.
+* **v0.0.1**: Got a button, and clicking it increments a variable which then updates on-screen. Some small quality-of-life improvements, such as app version in the page title, better layout, etc. Does not survive page reloads.
+
+
+## License
+
+You probably shouldn't use this code as the basis for anything, but if you do, it's licensed under the GNU General Public License v3.0, a copy of which is included in the repository.
--- game.js
+++ game.js
@@ -1,1 +1,53 @@
-console.log('Hello, devtools!');
+// Constants.
+const appName = 'Dumb Idle Game',
+    appVersion = '0.0.1',
+    appBuild = '2022-09-03',
+    debug = true;
+
+// Variables.
+let clicks = 0,
+    btnClick = document.querySelector('#btnClick'),
+    divClicks = document.querySelector('#divClicks'),
+    title = document.querySelector('#title');
+
+// Event listeners --------------------------------------------------------------------------------
+
+// Click button event listener.
+btnClick.addEventListener('click', (event) => {
+    clicks++
+
+    updateDivClicks()
+});
+
+// Functions --------------------------------------------------------------------------------------
+
+// Displays application version info.
+function getVersion() {
+    return appName + ' v' + appVersion + ' (' + appBuild + ').';
+}
+
+// Updates the div displaying the number of clicks.
+function updateDivClicks() {
+    divClicks.innerHTML = clicks
+
+    if (debug) {
+        console.log('Clicks: ' + clicks);
+    }
+};
+
+// Updates the page title with the current application version.
+function updateTitle() {
+    title.innerHTML = title.innerHTML + ' <small class="text-muted">v' + appVersion + '</small>'
+};
+
+// Initialisation things.
+function init() {
+    console.log('Welcome to ' + getVersion());
+
+    updateTitle()
+    updateDivClicks()
+}
+
+// Code to run on startup -------------------------------------------------------------------------
+
+init();
